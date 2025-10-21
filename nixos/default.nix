b51e95d34a92d08a018968ec981bed05d985af68
Gabriel TESSIER <gabriel.tessier45@gmail.com>
2025-10-21 21:20:35 +0200

    test ajout user

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,11 +1,23 @@
-{ config, lib, pkgs, ...}:
+{ config, lib, pkgs, hostname, ...}:
 {
   imports = [
     ./configuration.nix
-    ./hardware-configuration.nix
+    #./hardware-configuration.nix
   ];
 
   #config = {
     
   #};
+
+  virtualisation.vmVariant = {
+    virtualisation = {
+      memorySize = 2048; # Use 2048MiB memory.
+      cores = 3;
+      graphics = false;
+    };
+  };
+  networking.hostName = hostname; # Define your hostname.
+  console.keyMap = lib.mkForce "fr";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ]; # Enabling flakes
+  system.stateVersion = "25.05"; # Don't change it bro
 }
