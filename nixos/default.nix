bd1fdbbea113f9cc4e3b25a6f4f87d6c6f11b67d
Gabriel TESSIER <gabriel.tessier45@gmail.com>
2025-10-21 21:37:39 +0200

    user fonctionne

--- nixos/default.nix
+++ nixos/default.nix
@@ -2,20 +2,20 @@
 {
   imports = [
     ./configuration.nix
-    #./hardware-configuration.nix
+    ./hardware-configuration.nix
   ];
 
   #config = {
     
   #};
 
-  virtualisation.vmVariant = {
-    virtualisation = {
-      memorySize = 2048; # Use 2048MiB memory.
-      cores = 3;
-      graphics = false;
-    };
-  };
+  #virtualisation.vmVariant = {
+  #  virtualisation = {
+  #    memorySize = 2048; # Use 2048MiB memory.
+  #    cores = 3;
+      #graphics = false;
+  #  };
+  #};
   networking.hostName = hostname; # Define your hostname.
   console.keyMap = lib.mkForce "fr";
   nix.settings.experimental-features = [ "nix-command" "flakes" ]; # Enabling flakes
