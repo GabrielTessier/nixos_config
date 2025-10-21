6a8e90f6cee8b1557386f2b2710be4c8731d28e2
Gabriel TESSIER <gabriel.tessier45@gmail.com>
2025-10-21 21:20:35 +0200

    test ajout user

---
+++ modules/system/users/default.nix
@@ -0,0 +1,41 @@
+{ config, lib, inputs, ... }:
+
+{
+  options = {
+    systemSettings = {
+      users = lib.mkOption {
+        description = "List of desktop users to create on the system";
+        type = lib.types.listOf lib.types.str;
+      };
+      adminUsers = lib.mkOption {
+        description = "List of desktop users to grant admin (sudo) access on the system";
+        type = lib.types.listOf lib.types.str;
+      };
+    };
+  };
+  config = {
+
+    users.users = builtins.listToAttrs
+      (map (user: {
+        name = user;
+        value = {
+          isNormalUser = true;
+          extraGroups = [ "networkmanager" "input" "dialout" "video" "render" ] ++ (lib.optionals (lib.any (x: x == user) config.systemSettings.adminUsers) [ "wheel" ]);
+          createHome = true;
+          initialPassword = "pass";
+        };
+      }) config.systemSettings.users);
+
+    #services.getty.autologinUser = "gabriel";
+
+    #home-manager.users = builtins.listToAttrs
+    #  (map (user: {
+    #    name = user;
+    #    value = {
+    #      home.username = user;
+    #      home.homeDirectory = "/home/"+user;
+    #    };
+    #  }) config.systemSettings.users);
+  };
+}
+
