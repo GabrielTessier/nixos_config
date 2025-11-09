a584cb993b93d81bcbfaf322b76c98d9c56354a6
Gabriel TESSIER <gabriel.tessier45@gmail.com>
2025-11-10 00:08:05 +0100

    suppression commentaire flake.nix

--- flake.nix
+++ flake.nix
@@ -1,95 +1,56 @@
 {
   description = "My system configuration";
-
+  
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
+    
     nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
     
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    
     #grub2-themes = {
     #  url = "github:vinceliuice/grub2-themes";
     #};
-
-    #nixos-hardware = {
-    #  url = "github:NixOS/nixos-hardware/master";
-    #};
   };
-
+  
   outputs = inputs@{ self, ... }:
-
-    let
-      # Change to suit your config
-      hostname = "nixos";
-      #usernames = [ "gabriel" ];
-      nix-folder = "/etc/nixos/new_nix";
-      # add your model from this list: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
-      pc = "dell-inspiron-14-5420";
-
-      system = "x86_64-linux";
-    in {
-
-      # nixos - system hostname
-      nixosConfigurations.${hostname} = inputs.nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          { config.networking.hostName = hostname; }
-          #{ config.systemSettings.users = usernames; }
-
-          inputs.nixos-facter-modules.nixosModules.facter
-          { config.facter.reportPath = ./facter.json; }
-          
-          inputs.home-manager.nixosModules.home-manager {
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-              inherit nix-folder;
-            };
-          }
-          ./nixos
-          ./modules/system
-          
-          #inputs.grub2-themes.nixosModules.default
-          #inputs.nixos-hardware.nixosModules.${pc}
-        ];
-        specialArgs = {
-          inherit inputs;
-          inherit hostname;
-        };
+  let
+    # Change to suit your config
+    hostname = "nixos";
+    #usernames = [ "gabriel" ];
+    nix-folder = "/etc/nixos/new_nix";
+    
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.${hostname} = inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        { config.networking.hostName = hostname; }
+        #{ config.systemSettings.users = usernames; }
+        
+        inputs.nixos-facter-modules.nixosModules.facter
+        { config.facter.reportPath = ./facter.json; }
+        
+        inputs.home-manager.nixosModules.home-manager {
+          home-manager.extraSpecialArgs = {
+            inherit inputs;
+            inherit nix-folder;
+          };
+        }
+        ./nixos
+        ./modules/system
+        
+        #inputs.grub2-themes.nixosModules.default
+      ];
+      specialArgs = {
+        inherit inputs;
+        inherit hostname;
       };
-
-      #homeConfigurations = builtins.listToAttrs
-      #  (map (username: {
-      #    name = username;
-      #    value = inputs.lib.nixosSystem {
-      #      inputs.home-manager.lib.homeManagerConfiguration = {
-      #        pkgs = inputs.nixpkgs.legacyPackages.${system};
-      #        modules = [
-      #          ./modules/user
-      #          ./nixos/home/${username}.nix
-      #        ];
-      #        extraSpecialArgs = {
-      #          inherit username;
-      #          inherit nix-folder;
-      #        };
-      #      };
-      #    };
-      #  }) usernames);
-      #homeConfigurations.gabriel = inputs.home-manager.lib.homeManagerConfiguration {
-      #  pkgs = inputs.nixpkgs.legacyPackages.${system};
-      #  modules = [
-      #    ./modules/user
-      #    ./nixos/home/gabriel.nix
-      #  ];
-      #  extraSpecialArgs = {
-      #    username = "gabriel";
-      #    inherit nix-folder;
-      #  };
-      #};
     };
+  };
 }
   
