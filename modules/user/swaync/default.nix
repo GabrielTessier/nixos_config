d07900697efc4c80c92680e817f5d5f6177c36f8
Gabriel TESSIER <gabriel.tessier45@gmail.com>
2025-11-10 23:03:42 +0100

    swaync

---
+++ modules/user/swaync/default.nix
@@ -0,0 +1,101 @@
+{ config, lib, pkgs, inputs, ... }:
+let
+  cfg = config.userSettings.swaync;
+in
+{
+  options = {
+    userSettings.swaync = {
+      enable = lib.mkEnableOption "Enable swaync";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.swaync = {
+      enable = true;
+      style = ./style.css;
+      settings = {
+        "$schema" = "/etc/nixox/new_nix/modules/user/swaync/configSchema.json";
+        positionX = "right";
+        positionY = "top";
+        layer = "overlay";
+        control-center-layer = "top";
+        layer-shell = true;
+        cssPriority = "user";
+        control-center-margin-top = 13;
+        control-center-margin-bottom = 13;
+        control-center-margin-right = 14;
+        control-center-margin-left = 0;
+        notification-2fa-action = true;
+        notification-inline-replies = false;
+        notification-icon-size = 24;
+        notification-body-image-height = 100;
+        notification-body-image-width = 100;
+        notification-window-width = 300;
+        timeout = 6;
+        timeout-low = 3;
+        timeout-critical = 0;
+        fit-to-screen = true;
+        control-center-width = 400;
+        control-center-height = 720; # Ignored because 'fit-to-screen' is set to 'true'
+        keyboard-shortcuts = true;
+        transition-time = 200;
+        hide-on-clear = false;
+        hide-on-action = true;
+        script-fail-notify = true;
+        widgets = [
+          "dnd"
+          "buttons-grid"
+          "mpris"
+          "volume"
+          "backlight"
+          "title"
+          "notifications"
+        ];
+        widget-config = {
+          title = {
+            text = "Notifications";
+            clear-all-button = true;
+            button-text = "Clear";
+          };
+          dnd = {
+            text = "Do Not Disturb";
+          };
+          label = {
+            max-lines = 1;
+            text = "Notification";
+          };
+          mpris = {
+            image-size = 50;
+            image-radius = 0;
+          };
+          volume = {
+            label = "󰕾";
+          };
+          backlight = {
+            label = "󰃟";
+          };
+          buttons-grid = {
+            actions = [
+              {
+                label = "";
+                command = "hyprlock";
+              }
+              {
+                label = "󰝟";
+                command = "pamixer -t";
+              }
+              {
+                label = "󰂯";
+                command = "swaync-client -cp; blueman-manager";
+              }
+              {
+                label = "";
+                command = "wlogout";
+              }
+            ];
+          };
+        };
+      };
+    };
+  };
+}
