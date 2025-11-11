7190b3ee34455f45121db4cb5c7ea12e4a12e322
Gabriel TESSIER <gabriel.tessier45@gmail.com>
2025-11-11 01:08:41 +0100

    correction wlogout

--- modules/user/wlogout/wlogout.nix
+++ modules/user/wlogout/wlogout.nix
@@ -26,34 +26,36 @@ in
       enable = true;
       layout = [
         {
-          "label" = "lock";
-          "action" = "power-script lock";
-          "text" = "Lock";
-          "keybind" = "l";
-        }
-        {
-          "label" = "logout";
-          "action" = "power-script exit";
-          "text" = "Log Out";
-          "keybind" = "e";
-        }
-        {
           "label" = "suspend";
-          "action" = "power-script suspend";
+          "action" = "systemctl suspend";
           "text" = "Suspend";
           "keybind" = "u";
         }
         {
           "label" = "reboot";
-          "action" = "power-script reboot";
+          "action" = "systemctl reboot";
           "text" = "Restart";
           "keybind" = "r";
         }
         {
           "label" = "shutdown";
-          "action" = "power-script shutdown";
+          "action" = "systemctl poweroff";
           "text" = "Power Off";
           "keybind" = "s";
+        }
+      ]
+      ++ lib.optionals config.userSettings.hyprland.enable [
+        {
+          "label" = "logout";
+          "action" = "${pkgs.hyprland}/bin/hyprctl dispatch exit";
+          "text" = "Log Out";
+          "keybind" = "e";
+        }
+        {
+          "label" = "lock";
+          "action" = "${pkgs.hyprlock}/bin/hyprlock";
+          "text" = "Lock";
+          "keybind" = "l";
         }
       ];
       style = ./style.css;
