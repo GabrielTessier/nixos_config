6e57a55f29ea046a2d5d29fd03a718a10552bee3
Gabriel TESSIER <gabriel.tessier45@gmail.com>
2025-11-11 17:28:10 +0100

    nouvelle config emacs

--- modules/user/emacs/default.nix
+++ modules/user/emacs/default.nix
@@ -11,13 +11,23 @@ in
 
   config = lib.mkIf cfg.enable {
     # Utilisation de home-manager pour copier les fichiers configurations
-    home.file.".emacs.d/conf_file" = {
+    home.file.".emacs.d/old_config" = {
       source = ./conf_file;
     };
+
+    home.file.".emacs.d/my_init.el" = {
+      source = ./configuration/my_init.el;
+    };
+
+    home.file.".emacs.d/configuration.org" = {
+      source = ./configuration/configuration.org;
+    };
+    home.file.".emacs.d/resources".source = ./configuration/resources;
+    home.file.".emacs.d/themes".source = ./configuration/themes;
     
-	  programs.emacs = {
-		  enable = true;
-		  extraConfig = ''(load-file (expand-file-name "conf_file/init.el" user-emacs-directory))'';
+    programs.emacs = {
+      enable = true;
+      extraConfig = ''(load-file (expand-file-name "my_init.el" user-emacs-directory))'';
     };
   };
 }
